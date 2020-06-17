a71c10b308bff3f01dfbf902383136115f8df6ee
danamurphy48 <danamurphy48@gmail.com>
2020-06-17 17:08:23 -0500

    Added CompanyNotes model.

---
+++ ApplicationTracker/ApplicationTracker/Models/CompanyNotes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Models
+{
+    public class CompanyNotes
+    {
+        [Key]
+        public int CompanyNotesId { get; set; }
+        public string Description { get; set; }
+        public string Product { get; set; }
+        public string Culture { get; set; }
+        [Display(Name ="Recent News")]
+        public string RecentNews { get; set; }
+        public string Charity { get; set; }
+    }
+}
