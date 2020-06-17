6640823b4db58d75bab8fe7bf1ed652ac8ea8832
danamurphy48 <danamurphy48@gmail.com>
2020-06-17 17:58:07 -0500

    Added Hiring Manager model, fix datatypes. Added Applicants View - Index based on Application model. To do: add remaining tables and FKs.

---
+++ ApplicationTracker/ApplicationTracker/Models/HiringManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Models
+{
+    public class HiringManager
+    {
+        [Key]
+        public int HiringManagerId { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+    }
+}
