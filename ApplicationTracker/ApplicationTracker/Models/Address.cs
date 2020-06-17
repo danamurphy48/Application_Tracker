50df87b7ca53858d6ce658d59a85adaa65f08583
danamurphy48 <danamurphy48@gmail.com>
2020-06-17 17:04:31 -0500

    Added Address model

---
+++ ApplicationTracker/ApplicationTracker/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressId { get; set; }
+        [Display(Name ="Street Address")]
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name ="Zip Code")]
+        public int? ZipCode { get; set; }
+    }
+}
