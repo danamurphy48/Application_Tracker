f8fb86eedbf89adbdf61376f05a9f6571be7ba89
danamurphy48 <danamurphy48@gmail.com>
2020-06-18 10:03:05 -0500

    Added FKs to Company model.

--- ApplicationTracker/ApplicationTracker/Models/Company.cs
+++ ApplicationTracker/ApplicationTracker/Models/Company.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +15,21 @@ namespace ApplicationTracker.Models
         public string CompanyName { get; set; }
         [Display(Name ="Office Location")]
         public string OfficeLocation { get; set; }
+
+        [ForeignKey("Application")]
+        public int ApplicationId { get; set; }
+        public Application Application { get; set; }
+
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+        [ForeignKey("HiringManager")]
+        public int HiringManagerId { get; set; }
+        public HiringManager HiringManager { get; set; }
+
+        [ForeignKey("CompanyNotes")]
+        public int CompanyNotesId { get; set; }
+        public CompanyNotes CompanyNotes { get; set; }
     }
 }
