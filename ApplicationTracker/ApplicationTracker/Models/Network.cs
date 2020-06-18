be214329c0808025d2d73c53f8580db39803cdc2
danamurphy48 <danamurphy48@gmail.com>
2020-06-18 09:01:38 -0500

    Added Network Model

---
+++ ApplicationTracker/ApplicationTracker/Models/Network.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Models
+{
+    public class Network
+    {
+        [Key]
+        public int NetworkId { get; set; }
+        [Display(Name ="Name")]
+        public string NetworkName { get; set; }
+        [Display(Name ="Contact's Job Title")]
+        public string NetworkPosition { get; set; }
+    }
+}
