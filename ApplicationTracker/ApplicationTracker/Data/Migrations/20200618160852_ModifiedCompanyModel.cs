41f565332241d1b8fcba1ca034d9b74e0b7b5029
danamurphy48 <danamurphy48@gmail.com>
2020-06-18 11:13:57 -0500

    Added migration ModifiedCompanyModel. Need to review nullable FKs.

---
+++ ApplicationTracker/ApplicationTracker/Data/Migrations/20200618160852_ModifiedCompanyModel.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApplicationTracker.Data.Migrations
+{
+    public partial class ModifiedCompanyModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "70564b18-df68-4be2-8cb8-d01a004e313c");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Applicants",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StreetAddress = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyNotes",
+                columns: table => new
+                {
+                    CompanyNotesId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    Product = table.Column<string>(nullable: true),
+                    Culture = table.Column<string>(nullable: true),
+                    RecentNews = table.Column<string>(nullable: true),
+                    Charity = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyNotes", x => x.CompanyNotesId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HiringManagers",
+                columns: table => new
+                {
+                    HiringManagerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HiringManagers", x => x.HiringManagerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interviewers",
+                columns: table => new
+                {
+                    InterviewerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interviewers", x => x.InterviewerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobInformations",
+                columns: table => new
+                {
+                    JobInfoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Shift = table.Column<string>(nullable: true),
+                    FullTime = table.Column<string>(nullable: true),
+                    Contract = table.Column<string>(nullable: true),
+                    JobSalary = table.Column<string>(nullable: true),
+                    PositionSummary = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobInformations", x => x.JobInfoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Networks",
+                columns: table => new
+                {
+                    NetworkId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NetworkFirstName = table.Column<string>(nullable: true),
+                    NetworkLastName = table.Column<string>(nullable: true),
+                    NetworkPosition = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Networks", x => x.NetworkId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(nullable: true),
+                    OfficeLocation = table.Column<string>(nullable: true),
+                    AddressId = table.Column<int>(nullable: false),
+                    CompanyNotesId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyId);
+                    table.ForeignKey(
+                        name: "FK_Companies_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Companies_CompanyNotes_CompanyNotesId",
+                        column: x => x.CompanyNotesId,
+                        principalTable: "CompanyNotes",
+                        principalColumn: "CompanyNotesId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    ApplicationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PositionTitle = table.Column<int>(nullable: false),
+                    DateApplied = table.Column<DateTime>(nullable: false),
+                    ResumeUsed = table.Column<string>(nullable: true),
+                    CoverLetter = table.Column<string>(nullable: true),
+                    SalaryRequested = table.Column<double>(nullable: true),
+                    RequistionNumber = table.Column<double>(nullable: true),
+                    JobUrl = table.Column<string>(nullable: true),
+                    ApplicationStatus = table.Column<string>(nullable: true),
+                    CompanyId = table.Column<int>(nullable: false),
+                    NetworkId = table.Column<int>(nullable: false),
+                    ApplicantId = table.Column<int>(nullable: false),
+                    JobInfoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.ApplicationId);
+                    table.ForeignKey(
+                        name: "FK_Applications_Applicants_ApplicantId",
+                        column: x => x.ApplicantId,
+                        principalTable: "Applicants",
+                        principalColumn: "ApplicantId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_JobInformations_JobInfoId",
+                        column: x => x.JobInfoId,
+                        principalTable: "JobInformations",
+                        principalColumn: "JobInfoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_Networks_NetworkId",
+                        column: x => x.NetworkId,
+                        principalTable: "Networks",
+                        principalColumn: "NetworkId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interviews",
+                columns: table => new
+                {
+                    InterviewId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    InterviewType = table.Column<string>(nullable: true),
+                    InterviewLocation = table.Column<string>(nullable: true),
+                    InterviewRound = table.Column<int>(nullable: true),
+                    ThankYouNote = table.Column<bool>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    ApplicationId = table.Column<int>(nullable: false),
+                    HiringManagerId = table.Column<int>(nullable: false),
+                    InterviewerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interviews", x => x.InterviewId);
+                    table.ForeignKey(
+                        name: "FK_Interviews_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "ApplicationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Interviews_HiringManagers_HiringManagerId",
+                        column: x => x.HiringManagerId,
+                        principalTable: "HiringManagers",
+                        principalColumn: "HiringManagerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Interviews_Interviewers_InterviewerId",
+                        column: x => x.InterviewerId,
+                        principalTable: "Interviewers",
+                        principalColumn: "InterviewerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "253fe6b3-0111-400f-b210-6fcab0bdf5b9", "de7cc2da-b13a-4397-8bb7-aec52783298e", "Applicant", "APPLICANT" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_ApplicantId",
+                table: "Applications",
+                column: "ApplicantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_CompanyId",
+                table: "Applications",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_JobInfoId",
+                table: "Applications",
+                column: "JobInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_NetworkId",
+                table: "Applications",
+                column: "NetworkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CompanyNotesId",
+                table: "Companies",
+                column: "CompanyNotesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interviews_ApplicationId",
+                table: "Interviews",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interviews_HiringManagerId",
+                table: "Interviews",
+                column: "HiringManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interviews_InterviewerId",
+                table: "Interviews",
+                column: "InterviewerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interviews");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+
+            migrationBuilder.DropTable(
+                name: "HiringManagers");
+
+            migrationBuilder.DropTable(
+                name: "Interviewers");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "JobInformations");
+
+            migrationBuilder.DropTable(
+                name: "Networks");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "CompanyNotes");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "253fe6b3-0111-400f-b210-6fcab0bdf5b9");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Applicants");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "70564b18-df68-4be2-8cb8-d01a004e313c", "c4f3fa67-c6fa-452f-9397-70b08da2a3a7", "Applicant", "APPLICANT" });
+        }
+    }
+}
