ee6efa06cbccf0a08d586ed30af8ea269a95ef74
danamurphy48 <danamurphy48@gmail.com>
2020-06-23 18:31:31 -0500

    Removed commented out values from ApplicationIndexViewModel

--- ApplicationTracker/ApplicationTracker/Models/ApplicationIndexViewModel.cs
+++ ApplicationTracker/ApplicationTracker/Models/ApplicationIndexViewModel.cs
@@ -7,16 +7,6 @@ namespace ApplicationTracker.Models
 {
     public class ApplicationIndexViewModel
     {
-        //public Address Address { get; set; }
-        //public Applicant Applicant { get; set; }
-        //public Application Application { get; set; }
-        //public Company Company { get; set; }
-        //public CompanyNote CompanyNote { get; set; }
-        //public HiringManager HiringManager { get; set; }
-        //public Interview Interview { get; set; }
-        //public Interviewer Interviewer { get; set; }
-        //public JobInformation JobInformation { get; set; }
-        //public Network Network { get; set; }
         public List<Application> UpcomingApplications { get; set; }
         public List<Interview> UpcomingInterviews { get; set; }
     }
