e9978308f122bed0f7225b2294e2c7693ee3c2e0
danamurphy48 <danamurphy48@gmail.com>
2020-06-26 13:51:58 -0500

    Added Assessments Controller and seeded database for testing

---
+++ ApplicationTracker/ApplicationTracker/Controllers/AssessmentsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationTracker.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationTracker.Controllers
+{
+    [Authorize(Roles = "Applicant")]
+    public class AssessmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AssessmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: AssessmentsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AssessmentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AssessmentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AssessmentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AssessmentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AssessmentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AssessmentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AssessmentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
